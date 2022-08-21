631d31c780fd3f439079125dcbd631f492741f8e
Raphael Martins <raphael.almeida.martins@gmail.com>
2022-08-21 19:11:02 -0300

    :sparkles: feat: create matches table and models

---
+++ app/backend/src/database/models/Match.ts
@@ -0,0 +1,49 @@
+import * as Sequelize from 'sequelize';
+import db from '.';
+import Team from './Team';
+
+interface IMatch {
+  id: number;
+  homeTeam: number,
+  homeTeamGoals: number,
+  awayTeam: number,
+  awayTeamGoals: number,
+  inProgress: number,
+}
+
+type IMatchCreation = Omit<IMatch, 'id'>;
+
+class Match extends Sequelize.Model<IMatch, IMatchCreation> {
+  declare id: number;
+  declare homeTeam: number;
+  declare homeTeamGoals: number;
+  declare awayTeam: number;
+  declare awayTeamGoals: number;
+  declare inProgress: number;
+}
+
+Match.init({
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: Sequelize.INTEGER,
+  homeTeamGoals: Sequelize.INTEGER,
+  awayTeam: Sequelize.INTEGER,
+  awayTeamGoals: Sequelize.INTEGER,
+  inProgress: Sequelize.INTEGER,
+}, {
+  sequelize: db,
+  tableName: 'matches',
+  timestamps: false,
+  underscored: true,
+});
+
+Match.belongsTo(Team, { foreignKey: 'homeTeam' });
+Match.belongsTo(Team, { foreignKey: 'awayTeam' });
+Team.hasMany(Match, { foreignKey: 'id', as: 'matches' });
+
+export default Match;
+export { IMatch, IMatchCreation };
